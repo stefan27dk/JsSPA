fc69b0a10d91a3d1b1f3ac8f27a2d155cab9b42e
stefan27bg@abv.bg <stefan27bg@abv.bg>
2023-02-11 18:18:58 +0100

    Refined

--- wwwroot/static/js/main.js
+++ wwwroot/static/js/main.js
@@ -5,33 +5,34 @@ import Register from "./views/Register.js";
 
 
 
-const route = (e) => {
+const router = (e) => {
     e = event || window.event;
-    e.preventDefault(); // Prevent deafult behavior dont follow the link
-    window.history.pushState(null, null, e.target.href);
+    e.preventDefault(); // Prevent deafult behavior don't follow the link
+    window.history.pushState(null, null, e.target.href); // Add the url to the history api of js so we can navigate back and forth with the browser buttons
     handleLocation();
 };
 
 
+// The defined routes of the page
 const routes = {
-    '/Home': Home, // On Path "/" use the HomeView class and inject html in the #app div
+    '/': Home, // On Path "/" use the HomeView class and inject html in the #app div
     '/Login': Login,
     '/Register': Register };
 
  
 
-
+// The method that gets the current view and injects it in the "app"" container div.
 const handleLocation = async () => {
     const path = window.location.pathname; 
-    const route = routes[path] || routes['/Home'];
-    const html = await route.prototype.getHtml();
+    const currentRoute = routes[path] || routes['/Home']; // If there is no match go to Home "/" if the url is not found in the "routes object" than load home
+    const html = await currentRoute.prototype.getHtml();
     document.getElementById("app").innerHTML = html;
     //await route.prototype.executeViewScript();
 };
 
 
 // On-Navigating-Back&Forth-Load the Content--Together with the url------------------------------------------------------------------------------------>
-window.addEventListener("popstate", handleLocation); // On popstate "If back button is pressed" use the router array to load back the previeous SPA View
+window.addEventListener("popstate", handleLocation); // On popstate "If back button is pressed" use the method to load back the previeous SPA View
 //window.onpopstate = handleLocation;
 //window.route = route; // Assign router to Global variable so it can be acceset anywere instead can be exportet as module
 
@@ -43,13 +44,13 @@ window.addEventListener("popstate", handleLocation); // On popstate "If back but
 document.addEventListener("DOMContentLoaded", () => { // On Dom loaded add bodyEventlistener to listen for click in the body
     document.body.addEventListener("click", e => { //Listen for click in the body
         if (e.target.matches("[data-link]")) {  // If body item was clicked and it is data-link decorated 
-            route(); // Load the content if the url is defined in our "Spa Urls"
+            router(); // Load the content if the url is defined in our "Spa Urls"
         }
     });  
 });
 
 
-handleLocation(); // Handle location on page load otherwise the location will be handled on decorated link with data-link click or on popstate a.k.a on back and forth browser button click with window.addEventListener("popstate"
+handleLocation(); // Handle location on page load otherwise the location will be handled on decorated link with data-link click or on popstate a.k.a on back and forth browser button click with window.addEventListener("popstate"...
 
 
 
